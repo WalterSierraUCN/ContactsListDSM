5823e8807ecfbfed18cefd788429693879254bbb
Walter Sierra Vega <walter.sierra@alumnos.ucn.cl>
2021-06-18 02:33:55 -0400

    feat(ContactModel Implementation): Added contact model.

---
+++ app/src/main/java/clucn/disc/dsm/wsierra/contactlist/ContactModel.java
@@ -0,0 +1,45 @@
+package clucn.disc.dsm.wsierra.contactlist;
+
+/**
+ *
+ * @author Walter Sierra-Vega
+ */
+public class ContactModel {
+
+    // Initializes the name and the number.
+    String name, number;
+
+    /**
+     * Get the name.
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the name
+     * @param name
+     */
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the number
+     * @return
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * Set the number.
+     * @param number
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
+
